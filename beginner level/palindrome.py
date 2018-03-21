d950bb477da40b3081463b80f19f6168c9d13874
rahasudha2910 <37336116+rahasudha2910@users.noreply.github.com>
2018-03-21 09:34:08 +0530

    Create palindrome.py

---
+++ beginner level/palindrome.py
@@ -0,0 +1,7 @@
+num = raw_input("Enter any number: ") 
+rev_num = reversed(num) 
+if list(num) ==list(rev_num): 
+             print("Palindrome number") 
+else: 
+             print("Not Palindrome number")
+
